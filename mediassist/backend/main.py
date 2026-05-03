e5d36bbdfd8721b8dd03d482cf267aabc8cfe9a8
Manaswini Repalle <manaswinirepalle@example.com>
2026-05-03 12:26:08 +0530

    fix CORS origins

--- mediassist/backend/main.py
+++ mediassist/backend/main.py
@@ -34,7 +34,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://mediassist-git-main-manaswinirepalles-projects.vercel.app",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
