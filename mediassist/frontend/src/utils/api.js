c6b7fa9cfef5e655dd3d24266b2d68e5e26000e7
Manaswini Repalle <manaswinirepalle@example.com>
2026-05-03 14:55:14 +0530

    hardcore backend

--- mediassist/frontend/src/utils/api.js
+++ mediassist/frontend/src/utils/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
-
+const BASE_URL ='https://mediassist-9ibf.onrender.com'
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 30000,
